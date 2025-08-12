c9d3b50b5cafb48cd3e8b0061628aeab0b670bcb
Wendell Leão <leaowendell@outlook.com>
2025-08-12 16:35:33 -0300

    chore: delete unused helper

--- Runtime/Leaosoft.Utilities/Extensions/GameObjectExtensions.cs
+++ Runtime/Leaosoft.Utilities/Extensions/GameObjectExtensions.cs
@@ -10,23 +10,5 @@ namespace Leaosoft.Utilities.Extensions
 
             return gameObjectTransform.GetRootName();
         }
-        
-        public static bool TryFindObjectOfInterface<T>(out T result) where T : class
-        {
-            foreach (MonoBehaviour monoBehaviours in Object.FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None))
-            {
-                if (monoBehaviours is T type)
-                {
-                    result = type;
-                    return true;
-                }
-            }
-
-            // TODO:
-            Debug.LogError("TryFindObjectOfInterface");
-            
-            result = null;
-            return false;
-        }
     }
 }
